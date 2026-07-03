16626d1893f61b610645d2c18b81de252f7bff72
Monalisa Menezes <psimonafmenezes@gmail.com>
2026-07-03 17:27:19 -0300

    Initialize Devbank project with Maven setup and Spring Boot application structure

---
+++ src/main/java/com/monalisamenezes/devbank/DevbankApplication.java
@@ -0,0 +1,13 @@
+package com.monalisamenezes.devbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbankApplication.class, args);
+	}
+
+}
